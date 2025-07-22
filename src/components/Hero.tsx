340958b35d7286dbcc79d34d603fe7c1c5d871cb
Mohamadhossein4 <m.h.gholikhah@gmail.com>
2025-07-22 15:27:57 +0330

    some change

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -43,7 +43,7 @@ const Hero = () => {
 
               {/* Action Buttons */}
               <div className="flex flex-col sm:flex-row gap-4 xs:pb-10 lg:pb-0 justify-center lg:justify-start pt-4">
-                <a href="../../resume.pdf" download>
+                <a href="../../public/assets/resume.pdf" download>
                   <Button variant="hero" className="hover:bg-blue-500 hover:text-white rounded-full" size="lg">
                   <Download className="w-5 h-5 mr-2" />
                   دانلود رزومه
